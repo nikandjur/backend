0b7b708814a7e919928cd2510ed77d4ea91a0716
andrey <examle@gmail.com>
2025-06-20 02:54:02 +0300

    feat: fix

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -23,7 +23,7 @@ const setAuthCookie = (res: Response, sessionId: string) => {
 		httpOnly: false,
 		secure: true, 
 		sameSite: 'none', 
-		domain: '.blogpsy.ru',
+		domain: 'blogpsy.ru',
 		maxAge: 604800000, // 7 days
 	})
 }

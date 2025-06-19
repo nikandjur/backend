544bff519f8bf03d3e853e74a233a8bdd695d31d
andrey <examle@gmail.com>
2025-06-20 00:58:01 +0300

    feat: cors versel

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,9 +21,9 @@ import {
 const setAuthCookie = (res: Response, sessionId: string) => {
 	res.cookie('sessionId', sessionId, {
 		httpOnly: true,
-		secure: false, // true для HTTPS (в проде), false для HTTP (локально)
-		sameSite: 'lax', // или 'lax' для http
-		domain: 'dev.blogpsy.ru',
+		secure: true, 
+		sameSite: 'lax', 
+		domain: 'blogpsy.ru',
 		maxAge: 604800000, // 7 days
 	})
 }
